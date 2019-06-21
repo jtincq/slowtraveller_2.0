696fc094615cb5ef35a3d6215dbbc830f021c924
Justine Tincq <jtincq@gmail.com>
2019-06-21 13:22:10 +0200

    test setup

--- test/system/destinations_test.rb
+++ test/system/destinations_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class DestinationsTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test "visiting the home" do
     visit "/"
     assert_selector "h1", text: "Slow Traveller"
   end

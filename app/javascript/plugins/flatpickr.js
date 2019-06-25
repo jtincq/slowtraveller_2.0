8637c4e1e3cceac5333657ce9eb87c1ef4b7492b
Justine Tincq <jtincq@gmail.com>
2019-06-25 14:31:55 +0200

    flatpickr style

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,4 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/confetti.css" // A path to the theme CSS
 
 flatpickr(".datepicker", {})

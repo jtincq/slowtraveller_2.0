009d565ca17328a16329df409762433eadc6e3c0
Justine Tincq <jtincq@gmail.com>
2019-06-25 14:44:43 +0200

    departure date format

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,4 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/confetti.css" // A path to the theme CSS
 
-flatpickr(".datepicker", {minDate: "today"})
+flatpickr(".datepicker", {minDate: "today", dateFormat: "d-m-Y"})
